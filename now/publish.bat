f086e522333079841e3cf4e8081458630773c9be
Antonio Jr <acdcjunior@gmail.com>
2018-04-27 23:05:30 -0300

    chore: add pause

--- now/publish.bat
+++ now/publish.bat
@@ -1,3 +1,4 @@
 REM now is an npm package that was installed via `npm i -g now`. Login was saved in ~/.now
 now -e MONGO_LOGS_URI=@mongo-logs-uri --public
 echo Edit and run the command: now alias now-jrtdukayyh.now.sh c3pr-tool-walkmod-sonar
+pause
