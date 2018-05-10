a81b00f600b4e78a775d4046eb7bd3b108f38677
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-10 12:26:47 -0300

    fix: remove single quotes

--- now/run.bat
+++ now/run.bat
@@ -2,7 +2,7 @@ docker rm -f walkmod_sonar
 docker run ^
  -p 5003:5003 ^
  --name walkmod_sonar ^
- -e C3PR_AGENT_URL='http://127.0.0.1:5003' ^
- -e C3PR_AGENT_ID='walkmod-sonar' ^
- -e C3PR_REGISTRY_URL='http://host.docker.internal:5000/api/v1/registry' ^
+ -e C3PR_AGENT_URL=http://127.0.0.1:5003 ^
+ -e C3PR_AGENT_ID=walkmod-sonar ^
+ -e C3PR_REGISTRY_URL=http://host.docker.internal:5000/api/v1/registry ^
  acdcjunior/c3pr-tool-walkmod-sonar:1.4.0
