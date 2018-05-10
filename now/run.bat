b8c9acb7cd43c7cb653df1ec7cf76b700ceefb6b
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 10:53:13 -0300

    chore: pass required env vars

--- now/run.bat
+++ now/run.bat
@@ -1,1 +1,7 @@
-docker run -p 5003:5003 --name wsonar acdcjunior/c3pr-tool-walkmod-sonar:1.2.0
+docker run ^
+ -p 5003:5003 ^
+ --name wsonar ^
+ -e C3PR_AGENT_URL='http://127.0.0.1:5003' ^
+ -e C3PR_AGENT_ID='walkmod-sonar' ^
+ -e C3PR_REGISTRY_URL='http://host.docker.internal:5000' ^
+ acdcjunior/c3pr-tool-walkmod-sonar:1.2.0
