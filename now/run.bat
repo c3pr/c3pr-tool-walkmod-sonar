5992485b8d26eebda4c2cbb8e2ae42a69087bbff
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-10 11:34:58 -0300

    chore: update docker image to 1.4.0 tag

--- now/run.bat
+++ now/run.bat
@@ -1,7 +1,8 @@
+docker rm -f walkmod_sonar
 docker run ^
  -p 5003:5003 ^
- --name wsonar ^
+ --name walkmod_sonar ^
  -e C3PR_AGENT_URL='http://127.0.0.1:5003' ^
  -e C3PR_AGENT_ID='walkmod-sonar' ^
- -e C3PR_REGISTRY_URL='http://host.docker.internal:5000' ^
- acdcjunior/c3pr-tool-walkmod-sonar:1.2.0
+ -e C3PR_REGISTRY_URL='http://host.docker.internal:5000/api/v1/registry' ^
+ acdcjunior/c3pr-tool-walkmod-sonar:1.4.0
