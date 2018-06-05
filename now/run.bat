9d0a8dcc889496c1f1ae1a1e44b77c80c0a1394e
Antonio acdc Jr <acdcjunior@gmail.com>
2018-06-05 20:57:19 -0300

    chore: override hostnames

--- now/run.bat
+++ now/run.bat
@@ -2,7 +2,10 @@ docker rm -f walkmod_sonar
 docker run ^
  -p 5003:5003 ^
  --name walkmod_sonar ^
+ -e C3PR_HUB_URL=http://host.docker.internal:5000 ^
  -e C3PR_AGENT_URL=http://127.0.0.1:5003 ^
  -e C3PR_AGENT_ID=walkmod-sonar ^
  -e C3PR_REGISTRY_URL=http://host.docker.internal:5000/api/v1/registry ^
- acdcjunior/c3pr-tool-walkmod-sonar:1.4.0
+ --entrypoint "/bin/sh" ^
+ c3pr/c3pr-tool-walkmod-sonar:1.6.1 ^
+ -c "echo `/sbin/ip route|awk '/default/ { print $3 }'` c3prgitlab >> /etc/hosts; /c3pr/agent/c3pr-agent"
