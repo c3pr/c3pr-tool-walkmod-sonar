f435b14e1781773c54e797f14dded10fea1841ce
Antonio acdc Jr <acdcjunior@gmail.com>
2018-06-20 13:31:27 -0300

    chore: add C3PR_MONGO_URL to script

--- now/run.bat
+++ now/run.bat
@@ -2,6 +2,7 @@ docker rm -f walkmod_sonar
 docker run ^
  -p 5003:5003 ^
  --name walkmod_sonar ^
+ -e C3PR_MONGO_URL=%C3PR_MONGO_URL% ^
  -e C3PR_HUB_URL=http://host.docker.internal:5000 ^
  -e C3PR_AGENT_URL=http://127.0.0.1:5003 ^
  -e C3PR_AGENT_ID=walkmod-sonar ^
